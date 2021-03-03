4f1f3e5777abc3d89698470bda16dcdc8f5af94b
bijoya <bijoya10101@gmail.com>
2021-03-03 23:32:44 +0530

    isTouching

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
+var movingRect, staticRect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(windowWidth,windowHeight);
+  staticRect=createSprite(400, 200, 50, 50);
+  movingRect=createSprite(200, 200, 50, 80);
 }
 
 function draw() {
-  background(255,255,255);  
+  background(200);  
+
+  movingRect.x=mouseX;
+  movingRect.y=mouseY;
+
+  if(movingRect.x-movingRect.width/2<staticRect.x+staticRect.width/2){
+    staticRect.shapeColor="red"
+
+  }else{
+    staticRect.shapeColor="blue"
+  }
+
   drawSprites();
 }
